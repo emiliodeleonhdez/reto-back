210f1124b2de5ddd522c5351785b9ada2dbde5ef
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2021-11-04 20:58:56 -0600

    añadiendo conexión a base de datos

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const apiRouter = require("./routes");
+const db = require("./lib/db") // importa la conexión de la base de datos en .lib/db.js
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+/* apiRouter(app); */
+
+
+app.listen(port, () => {
+  console.log(`Listening on port: http://localhost:${port}`);
+  db.connect() // Aquí se establece la conexión con la base de datos
+    .then(()=>{
+      console.log("Data Base Connection stablished")
+    })
+    .catch((error)=>{ //
+      console.error("Connection refused", error)
+    })
+});
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const userRouter = require("./userRouter");
+const authRouter = require("./authRouter");
+const postRouter = require("./postRouter");
+
+const apiRouter = (app) => {
+  app.use("/users", userRouter);
+  app.use("/auth", authRouter);
+  app.use("/posts", postRouter);
+};
+
+module.exports = apiRouter;
